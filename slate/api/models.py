3e3fafa98eeae0ad23901b3f912db925e224201f
Matheus Santana <embs@cin.ufpe.br>
2018-07-06 10:46:58 -0300

    slate: add `done` to Task

--- slate/api/models.py
+++ slate/api/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class Task(models.Model):
     name = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
+    done = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
